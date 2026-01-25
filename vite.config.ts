5fe32aa3e6a5be7a283be4cdf04a9359823f197c
Okpainmo <okpainmoandrew@gmail.com>
2026-01-25 06:41:58 +0100

    delete: cleared react app - reverted back to nextjs - will build with github actions

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/Okpainmo.github.io/', // REQUIRED for GitHub Pages
-});
