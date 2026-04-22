fd495ecaa2bbfcb52906fb123aca37a7191609cb
Okpainmo <okpainmoandrew@gmail.com>
2026-04-22 21:12:43 +0100

    chore: new font for code-block enhancement in the post page(s)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import '@/app/styles/globals.css';
 import type { Metadata } from 'next';
-import { nunito_sans, poppins, lato } from './utils/font';
+import { nunito_sans, poppins, lato, jetbrains_mono } from './utils/font';
 import { Toaster } from 'react-hot-toast';
 import { AppProvider } from '@/app/rtk-base/provider';
 import { GoogleAnalytics } from '@/app/components/Analytics';
@@ -44,7 +44,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={`${nunito_sans} ${poppins} ${lato}`}>
+    <html
+      lang="en"
+      className={`${nunito_sans} ${poppins} ${lato} ${jetbrains_mono}`}
+    >
       <body suppressHydrationWarning>
         <AppProvider>
           <GoogleAnalytics />
--- app/utils/font.ts
+++ app/utils/font.ts
@@ -1,4 +1,4 @@
-import { Lato, Nunito_Sans, Poppins } from 'next/font/google';
+import { Lato, Nunito_Sans, Poppins, JetBrains_Mono } from 'next/font/google';
 
 const nunito_sans_init = Nunito_Sans({
   subsets: ['latin'],
@@ -21,6 +21,13 @@ const lato_init = Lato({
   weight: ['100', '300', '400', '700', '900']
 });
 
+const jetbrains_mono_init = JetBrains_Mono({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-jetbrains_mono'
+});
+
 export const nunito_sans = nunito_sans_init.variable;
 export const poppins = poppins_init.variable;
 export const lato = lato_init.variable;
+export const jetbrains_mono = jetbrains_mono_init.variable;
